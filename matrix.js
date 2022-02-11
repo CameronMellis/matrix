bdde3ae5f0a5df1df2c426a8aaa963ae91acbc4b
314562012 <88047548+CaMel-315@users.noreply.github.com>
2022-02-11 00:39:31 -0330

    functional

--- matrix.js
+++ matrix.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d"); // context
 
 let cw = window.innerWidth; // context width
-let ch = window.innerHeight; //context height
+let ch = window.innerHeight; // context height
 
 let charArr = [
     "a",
@@ -89,6 +89,34 @@ let charArr = [
           this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
 
           ctx.fillStyle = "rgba(0,255,0)";
-          
+          ctx.font = fontSize + "px san-serif";
+          ctx.fillText(this.value, this.x, this.y);
+          this.y += this.speed;
+
+          if (this.y > ch) {
+              this.y= (Math.random() *ch) / 2 - 50;
+              this.x = (Math.floor(Math.random() * maxColumns) * fontSize);
+              this.speed = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+          }
       }
   }
+
+  let update = () => {
+      if (fallingCharArr.length < maxCharCount) {
+          let fallingChar = new FallingChar(
+              Math.floor(Math.random() * maxColumns) * fontSize,
+              (Math.random() * ch) / 2 - 50
+          );
+          fallingCharArr.push(fallingChar);
+      }
+      ctx.fillStyle = "rgba(0,0,0,0.05)";
+      ctx.fillRect(0, 0, cw, ch);
+      for (let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
+          fallingCharArr[i].draw(ctx);
+      }
+      
+      requestAnimationFrame(update);
+      frames++;
+  };
+
+  update();
